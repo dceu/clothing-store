0de9697949cb390c37106533e927a63f5ea32cd8
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-02-16 22:49:07 -0600

    updated app with a shop page, collection preview component and collection item component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,16 @@
 
 import React from 'react'
-
+import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
+
 
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             { // performance concern because anon funcs are called everytime this comp is rendered
-                items.filter((item, idx) => idx < 4).map((item) => (
-                    <div key={item.id}>{item.name}</div>
+                items.filter((item, idx) => idx < 4).map(({ id, ...otherItemProps }) => (
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
         </div>
