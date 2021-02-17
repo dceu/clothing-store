4acaada65a4fa52a1340b462f3004a6d80ca8f5e
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-02-16 23:03:30 -0600

    created header component, logo.svg, and styles

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx'
+import Header from './components/header/header.component'
 
 
 function App() {
   return (
     // <HomePage />
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
