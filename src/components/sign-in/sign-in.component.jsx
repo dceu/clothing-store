304d374128834cf4de12b358c6448ce672ce456f
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-02-18 00:47:53 -0600

    wrap buttons in div

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -39,9 +39,11 @@ class SignIn extends React.Component {
 
                     <FormInput name="password" type="password" handleChange={this.handleChange} value={this.state.password} label="password" required />
 
+                    <div className='buttons'>
+                        <CustomButton type="submit" value="Submit Form" > Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn > Sign In with Google </CustomButton>
+                    </div>
 
-                    <CustomButton type="submit" value="Submit Form" > Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} > Sign In with Google </CustomButton>
                 </form>
             </div>
         )
