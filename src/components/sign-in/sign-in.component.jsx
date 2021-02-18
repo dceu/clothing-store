48acb0af770525f2411ae4355a4e40edcde26b1c
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-02-18 00:17:26 -0600

    OAuth and Firebase initial implementation

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,6 +5,8 @@ import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/filebase.utils.js'
+
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
@@ -39,6 +41,7 @@ class SignIn extends React.Component {
 
 
                     <CustomButton type="submit" value="Submit Form" > Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} > Sign In with Google </CustomButton>
                 </form>
             </div>
         )
