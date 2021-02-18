ce5d357fd3728706c5bb8085da26474280026939
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-02-18 00:35:05 -0600

    firename changes smh

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/filebase.utils.js'
+import { signInWithGoogle } from '../../firebase/firebase.utils.js'
 
 class SignIn extends React.Component {
     constructor(props) {
