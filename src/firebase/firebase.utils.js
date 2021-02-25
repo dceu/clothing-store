fee5093036826a0249ca8212156b4561681d2050
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-02-25 00:05:14 -0600

    firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     measurementId: "G-EKEG1ZN4XY"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    console.log(firestore.doc('users/'));
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+
+        }
+    };
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
