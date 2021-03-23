74102ca0af7aa3b5bdc33f7e9914153cf6e39756
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-03-23 16:08:05 -0500

    reformat of code

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,7 +9,9 @@ const CollectionPreview = ({ title, items }) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             { // performance concern because anon funcs are called everytime this comp is rendered
-                items.filter((item, idx) => idx < 4).map((item) => (
+                items
+                .filter((item, idx) => idx < 4)
+                .map((item) => (
                     <CollectionItem key={item.id} item={item} />
                 ))
             }
